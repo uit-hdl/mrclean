2452c3e701446300149447d989b4900b8efd8b1e
Giacomo Tartari <giacomo.tartari@uit.no>
2014-10-30 15:10:04 +0100

    somemore docs

---
+++ displaycloud/doc.go
@@ -0,0 +1,7 @@
+/*
+displaycloud provides basic functionality to interact with the DisplayCloud "middleware".
+
+The displaycloud package is used by mrclean to interact with the DislpayCloud middleware.
+It provides the basic functionalities to marshal and unmarshal the JSONRPC messages to be sent via websocket.
+*/
+package displaycloud
